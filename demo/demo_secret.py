fb9f7a3365fec92c5f4b76b917ba632a4ba9ffea
xhrg <634789257@qq.com>
2020-09-20 13:47:06 +0800

    修改单元测试
    修改单元测试

--- demo/demo_secret.py
+++ demo/demo_secret.py
@@ -12,7 +12,7 @@ apollo_config_url = os.environ.get("APOLLO_CONFIG_URL")
 
 print(apollo_config_url)
 
-client = ApolloClient(app_id="demo-service", cluster="default", apollo_config_url=apollo_config_url,
+client = ApolloClient(app_id="demo-service", cluster="default", config_url=apollo_config_url,
                       secret="cdbb0b1b67d94a63a20f9002c092e6cf")
 val = client.get_value("name", default_val="defaultVal")
 
