565df48885420e09fe6c74a8bd1b226c2d85f3e8
陈华杰 <cmathking@gmail.com>
2023-04-03 18:53:53 +0800

    Update setup.py
    包名改为apollo-client-python

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='python-apollo-client',  # Required
+    name='apollo-client-python',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
